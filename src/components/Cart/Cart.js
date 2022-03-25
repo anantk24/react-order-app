37113e3c78622eeba0bb1d318cf6005e909541c1
anant <anantk24>
2022-03-25 23:30:53 +0530

    update card , removed dollar sign

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,7 +10,7 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <Modal>
+    <Modal onClose={props.onCancelCart}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,7 +2,7 @@ import classes from "./Modal.module.css";
 import React from "react";
 import ReactDom from "react-dom";
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 const ModelOverlay = (props) => {
   return (
@@ -15,9 +15,12 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <React.Fragment>
-      {ReactDom.createPortal(<Backdrop />, portalElement)}
       {ReactDom.createPortal(
-        <ModelOverlay>${props.children}</ModelOverlay>,
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDom.createPortal(
+        <ModelOverlay>{props.children}</ModelOverlay>,
         portalElement
       )}
     </React.Fragment>
